4c575b06807a587814e6ba6940353652ce0df2f2
midK-night <130721380+midK-night@users.noreply.github.com>
2024-05-13 22:56:50 -0500

    created Team class

---
+++ src/main/java/org/example/Team.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Team {
+    private String name;
+    private Date date;
+    private ArrayList<String> team;
+    private ArrayList<Integer> draftOrder;
+
+    public Team(String name, Date date, ArrayList<String> team, ArrayList<Integer> draftOrder) {
+        this.name = name;
+        this.date = date;
+        this.team = team;
+        this.draftOrder = draftOrder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ArrayList<String> getTeam() {
+        return team;
+    }
+
+    public ArrayList<Integer> getDraftOrder() {
+        return draftOrder;
+    }
+}
